04848ff2f1938a2b98a4628c4b47cfb42e1e2e8f
Luis Mendes <machadoluismendes@gmail.com>
2022-12-14 12:27:37 -0300

    Increment
    One mobile frontend kotlin Android Project
    One api Spring Initializr IntelliJ Project

---
+++ dioeducationformacoes/src/main/kotlin/com/challenge/dioeducationformacoes/dioeducationformacoes/DioeducationformacoesApplication.kt
@@ -0,0 +1,11 @@
+package com.challenge.dioeducationformacoes.dioeducationformacoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DioeducationformacoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DioeducationformacoesApplication>(*args)
+}
---
+++ dioeducationformacoes/src/test/kotlin/com/challenge/dioeducationformacoes/dioeducationformacoes/DioeducationformacoesApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.challenge.dioeducationformacoes.dioeducationformacoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DioeducationformacoesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		println("api")
+	}
+
+}
